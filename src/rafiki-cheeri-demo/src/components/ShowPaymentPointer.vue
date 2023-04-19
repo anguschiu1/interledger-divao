e881800ff757a69643da5e551a18be5c32cadd18
Angus Chiu <anguschiu1@users.noreply.github.com>
2023-04-19 23:51:09 +0100

    Fetch paymentpointer from OpenPayment API

--- src/rafiki-cheeri-demo/src/components/ShowPaymentPointer.vue
+++ src/rafiki-cheeri-demo/src/components/ShowPaymentPointer.vue
@@ -1,4 +1,6 @@
 <script>
+import { onMounted } from 'vue'
+
 const API_URL = `http://localhost:8080/gfranklinpp`
 export default {
   props: {
@@ -8,6 +10,9 @@ export default {
     commits: null,
     fetchedData: null
   }),
+  created() {
+    // this.fetchData()
+  },
   methods: {
     async fetchData() {
       var url = `${API_URL}`
@@ -30,7 +35,7 @@ export default {
       await fetch(url, options)
         .then((response) => response.text())
         .then((result) => {
-          this.fetchedData = result
+          this.fetchedData = JSON.parse(result)
           console.log(result)
         })
         .catch((error) => console.log('error', error))
@@ -41,12 +46,27 @@ export default {
 
 <template>
   <div class="greetings">
-    <h3 v-show="fetchedData">
-      Payment Pointer Transactions of accounts "gfranklin" is shown here.
+    <h3 v-if="this.fetchedData == null">
+      Payment Pointer Transactions of accounts "gfranklin" should be shown here.
     </h3>
-    {{ this.fetchedData }}
+    <table v-if="this.fetchedData !== null" class="styled-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Payment Pointer</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ this.fetchedData.publicName }}</td>
+          <td>{{ this.fetchedData.id ? this.fetchedData.id : '' }}</td>
+          <td>{{ this.fetchedData.assetCode ? this.fetchedData.assetCode : '' }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <div class="fetchURL">
+  <div class="fetchURL" v-show="!fetchedData">
     <button @click="fetchData">Fetch Data</button>
   </div>
 </template>
@@ -66,5 +86,29 @@ export default {
   font-family: sans-serif;
   min-width: 400px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
 }
 </style>
